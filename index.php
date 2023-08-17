b671e5da564f094d3fc0b59d2f0a880c5bb93975
tasyakaos <taykasks@gmail.com>
2023-08-16 21:31:24 -0400

    first_project

---
+++ index.php
@@ -0,0 +1,180 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ANDY'S BARBERSHOP</title>
+    <link rel="stylesheet" href="css\style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,100;1,200&display=swap" rel="stylesheet">
+</head>
+<body> 
+
+<div class="notice main-notice">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-10 offset-md-1 col-12 offset-0 text-center">
+              <p style="font-family:'Adlery Swash';">We will make your style!
+                <br>
+              Join our family!
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+      <nav class="navbar navbar-expand-xl fixed-top homepage-nav black-logo" style="top: 66px;">
+      <div class="navbar-logo logo-dark">
+            <a href=""><img src="" alt="" width="150"></a>
+          </div>
+      <div class="container-fluid">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".navbar-collapse" aria-expanded="true" aria-label="Toggle navigation"> 
+            <span class="menu-toggle"></span> </button>
+          <div id="main-nav" class="collapse navbar-collapse justify-content-end">
+            <ul id="menu-main-nav" class="navbar-nav">
+              <li id="menu-item-76" class="nav-item nav-parent menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-76 dropdown">
+              <li id="menu-item-77" class="nav-item nav-parent menu-item menu-item-type-post_type menu-item-object-page menu-item-77"><a title="Location" href="#Location" class="nav-link">Location</a></li>
+              <li id="menu-item-77" class="nav-item nav-parent menu-item menu-item-type-post_type menu-item-object-page menu-item-77"><a title="App" href="#App" class="nav-link">App</a></li>
+              <li id="menu-item-889" class="nav-item menu-item menu-item-type-custom menu-item-object-custom menu-item-889"><a title="Services" href="#Services" class="nav-link">Services</a></li>
+              <li id="menu-item-424" class="nav-item nav-parent menu-item menu-item-type-post_type menu-item-object-page menu-item-424"><a title="Appointment" href="#Appointment" class="nav-link">Appointment</a></li>
+</ul></div>        </div>
+      </nav>
+
+      <section>
+        <div class="section-one">
+      <h1 style="font-family:'Adlery Swash';">Welcome to Our First Barbershop!</h1> 
+        <br>
+        <h2 style="font-family:'Adlery Swash';">Step into a world of style and comfort, where your grooming experience takes center stage.</h2>
+        <h2 style="font-family:'Adlery Swash';">We're thrilled to have you here and can't wait to create the perfect look that reflects your unique personality. </h2><br>
+        <h3 style="font-family:'Adlery Swash';">Our team is dedicated to providing top-notch service, attention to detail, and a warm atmosphere that makes you feel right at home. </h3><br>
+        <p class="subtitle" style="font-family:'Adlery Swash';">Thank you for choosing us – let's embark on this style journey together!</p> 
+        </div>
+      </section>
+
+      <section>
+        <div class="section-two">
+        <img src="img\Andys.jpg" style="height: 600px; width: 500px;" alt="ph">
+        </div>
+      </section>
+
+      <section>
+        <div class="section-three">
+          <h2 style="font-family:'Adlery Swash';">From a law student to a master of the clipper, my journey took an unexpected turn...</h2> <br>
+           <h3 style="font-family:'Adlery Swash';">Ukraine shaped my barbering skills over three dedicated years, perfecting a clean technique that blends precision with artistry.</h3> <br>
+           <h4 style="font-family:'Adlery Swash';">Fueled by an unwavering passion for the craft, I transitioned from legal books to barber tools, embracing a fulfilling path that speaks volumes through every cut!</h4>
+        </div>
+      </section>
+
+      <section>
+        <div class="section-four">
+          <img src="img\photo.jpg" style="width: 100%; height: 50%">
+        </div>
+      </section>
+
+      <section>
+        <div class="section-five">
+          <h2 style="width: 70%; margin-left: 100px; font-family:'Adlery Swash';">Step into our barbershop and instantly feel the cozy embrace of a welcoming atmosphere..</h2><br>
+           <h4 style="width: 70%; margin-left: 100px; font-family:'Adlery Swash';">Experience grooming sessions that redefine relaxation and create a truly enjoyable escape</h4><br>
+           <h4 style="width: 70%; margin-left: 100px; font-family:'Adlery Swash';">Beyond the chair, we're not just barbers – we're creators of connections, building lasting relationships one conversation at a time!</h4>
+        </div>
+      </section>
+      
+      <section>
+        <div class="section-six">
+          <h2 style="width: 70%; margin-left: 100px; font-family:'Adlery Swash';">Meet our exceptional team of skilled barbers, each hailing from diverse backgrounds! </h2> <br>
+          <h4 style="width: 70%; margin-left: 100px; font-family:'Adlery Swash';">Our commitment to continuous learning ensures we're always up-to-date with the latest trends and techniques, <br>
+            offering you a wide spectrum of options to perfectly match your unique style preferences.</h4>
+            <h4 style="width: 70%; margin-left: 100px; margin-top: 30px; font-family:'Adlery Swash';">...</h4>
+        </div>
+      </section>
+
+      <section>
+        <div class="section-seven">
+          <article id="Services"><img src="img\1.jpg" style="height: 300px; width: 300px; margin-left: 150px; margin-top: 150px;">
+          <h4 style="height: 300px; width: 300px; margin-left: 150px; margin-top: 30px; text-align:center; font-family:'Adlery Swash';">
+Men’s cut: $15 to $60 <br>
+Kids’ cut:  $15 to $40 <br>
+Trim: $5 to $115 <br>
+Styling:	$10 to $65 <br>
+Cut and color: $20 to $125 <br>
+Cut and style:	$50 to $190</h4>
+        </div>
+      </section>
+
+
+      <section>
+        <div class="section-eight">
+          <h2 style="margin-left: 0px; margin-top: 50px; text-align:center; font-family:'Adlery Swash';">SERVICES</h2>
+        <img src="img\2.jpg" style="height: 300px; width: 300px; margin-left: 150px; margin-top: 50px;">
+        <h4 style="height: 300px; width: 300px; margin-left: 150px; margin-top: 30px; text-align:center; font-family:'Adlery Swash';">
+Beard trim: $5 to $15 <br>
+Fade: $30 to $40 <br>
+Color treatment: $35 to $80 <br>
+Dreadlocks: $48 to $1,400 <br>
+Consultation:	Free to $50 <br>
+Tip: 10% to 25%
+      </h4>
+        </div>
+      </section>
+
+
+      <section>
+        <div class="section-nine">
+        <img src="img\3.jpg" style="height: 300px; width: 300px; margin-left: 150px; margin-top: 150px;">
+        <h4 style="height: 300px; width: 300px; margin-left: 150px; margin-top: 30px; text-align:center; font-family:'Adlery Swash';">
+Updos:	$50 and $130 <br>
+Blowout:	$20 to $100 <br>
+Braids:	$40 to $200 <br>
+Locs:	$50 to $150 <br>
+Hair color: $75 to $200 <br>
+Balayage: $75 to $450 <br>
+Bleach: $50 to $400 <br>
+Special occasion style: $40 to $325 <br>
+Perm:	$30 to $800 <br>
+      </h4>
+</article>
+        </div>
+      </section>
+
+
+      <section>
+        <div class="section-ten">
+          <img src="img\0.jpg" style="height: 600px; width: 500px; margin-left: 100px; margin-top: 100px;">
+        </div>
+      </section>
+    
+
+      <section>
+        <div class="section-eleven">
+        <h4 style="width:70%; margin-left: 100px; margin-top: 250px; text-align:center; font-family:'Adlery Swash';">
+        Embark on your style journey with us – book your appointment now and let's craft perfection together. Your desired look is just a click away!</h4>
+
+        <article id="Appointment"><button id="squire-book-button" style="transition: opacity 300ms linear 300ms; visibility: visible; display: unset; opacity: 1; margin-left: 350px; margin-top: 30px;"><a href="BookOnline" style="color: black; text-decoration: none; font-family:'Adlery Swash'; font-size: 19px;">Book online</a></button></article>
+
+        <h4 style="width:70%; margin-left: 300px; margin-top: 200px; text-align:center; font-family:'Adlery Swash';">+1xxxxxxxxxx</h4>
+        </div>
+      </section>
+
+
+      <section>
+        <div class="section-twelve">
+        <article id="Location"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d220364.98661259946!2d-81.84825801569302!3d30.345182535562234!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88e5b716f1ceafeb%3A0xc4cd7d3896fcc7e2!2sJacksonville%2C%20FL!5e0!3m2!1sen!2sus!4v1691948911487!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></article>
+        </div>
+      </section>
+
+
+      <section>
+        <div class="section-thirteen">
+          <article id="App"><img src="img\Vector.png" style="margin-top: 100px; margin-left: 300px; width: 450px; height: 450px;"></article> <!-- example -->
+        </div>
+      </section>
+
+
+
+
+
+    </body>
+</html>
